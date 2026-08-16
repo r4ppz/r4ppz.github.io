eded6572ceacb2295bd3aae51ed84de0d613241b
r4ppz <johnreyrabosa.f@gmail.com>
2026-08-16 13:55:44 +0800

    chore(nextConfig): remove staticGenerationRetryCount

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	output: 'export',
-	experimental: {
-		staticGenerationRetryCount: 3,
-	},
 };
 
 export default nextConfig;
